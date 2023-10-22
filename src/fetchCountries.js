79e5c4af431035559acfa0bcdb5a55bd40f7d21e
TomKrawczyk <tkrawczyk173@gmail.com>
2023-10-22 22:19:19 +0200

    modified:   package-lock.json 	modified:   src/fetchCountries.js 	modified:   src/index.html 	modified:   src/index.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,9 @@
-const BASE_URL = "https://restcountries.com/v3.1/name";
-
-export function fetchCountries(name) {
-    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
+﻿export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
             if (response.ok) {
                 return response.json();
-            } else {
-                throw new Error(`Failed to download country data for ${name}`);
             }
-        })
-        .catch(error => {
-            throw new Error(error);
+            throw new Error(response.statusText);
         });
-}
+};
--- src/index.js
+++ src/index.js
@@ -3,73 +3,66 @@ import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries.js';
 
 
-const searchInput = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
+const DEBOUNCE_DELAY = 300;
 
+const input = document.querySelector("#search-box");
+const list = document.querySelector(".country-list");
+const div = document.querySelector(".country-info");
 
-function renderCountryList(countries) {
-  const listItems = countries.map((country) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<img src="${country.flags.svg}" alt="${country.name.official}" /> ${country.name.official}`;
-    listItem.addEventListener('click', () => renderCountryInfo(country));
-    return listItem;
-  });
-  clearUI();
-  countryList.append(...listItems);
-}
+let searchCountryName = '';
 
-function renderCountryInfo(country) {
-  const countryDetails = `
-    <div class="country-info">
-      <img src="${country.flags.png}" alt="${country.name.common}" />
-      <h2>${country.name.common}</h2>
-      <p><b>Capital:</b> ${country.capital}</p>
-      <p><b>Population:</b> ${country.population}</p>
-      <p><b>Languages:</b> ${Object.values(country.languages).join(', ')}</p>
-    </div>
-  `;
-  clearUI();
-  countryInfo.innerHTML = countryDetails;
-}
+input.addEventListener("input", debounce(onInputChange, DEBOUNCE_DELAY));
 
-function clearUI() {
-  countryList.innerHTML = '';
-  countryInfo.innerHTML = '';
-}
-
-function onSearchInput() {
-  const searchValue = searchInput.value.trim();
-  if (searchValue === '') {
-    clearUI();
-    return;
-  }
-
-  fetchCountries(searchValue)
-    .then((countries) => {
-      clearUI();
-      if (!countries) {
-        return Notiflix.Notify.failure('Oops, there is no country with that name.');
-      }
-
-      if (countries.length > 10) {
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+function onInputChange() {
+    searchCountryName = input.value.trim();
+    if (searchCountryName === '') {
+        clearAll();
         return;
-      }
-
-      if (countries.length >= 2 && countries.length <= 10) {
-        renderCountryList(countries);
-        return;
-      }
-
-      if (countries.length === 1) {
-        renderCountryInfo(countries[0]);
-      }
+    } else fetchCountries(searchCountryName).then(countryNames => {
+        if (countryNames.length < 2) {
+            createCountrieCard(countryNames);
+            Notiflix.Notify.success('Here your result');
+        } else if (countryNames.length < 10 && countryNames.length > 1) {
+            createCountrieList(countryNames);
+            Notiflix.Notify.success('Here your results');
+        } else {
+            clearAll();
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        };
     })
-    .catch((error) => {
-      Notiflix.Notify.failure('An error occurred while fetching data.');
-      console.error(error);
+        .catch(() => {
+        clearAll();
+        Notiflix.Notify.failure('Oops, there is no country with that name.');
     });
-}
+};
 
-searchInput.addEventListener('input', debounce(onSearchInput, 300));
+function createCountrieCard(country) {
+    clearAll();
+    const c = country[0];
+    const readyCard = `<div class="country-card">
+        <div class="country-card--header">
+            <img src="${c.flags.svg}" alt="Country flag" width="55", height="35">
+            <h2 class="country-card--name"> ${c.name.official}</h2>
+        </div>
+            <p class="country-card--field">Capital: <span class="country-value">${c.capital}</span></p>
+            <p class="country-card--field">Population: <span class="country-value">${c.population}</span></p>
+            <p class="country-card--field">Languages: <span class="country-value">${Object.values(c.languages).join(',')}</span></p>
+    </div>`
+    div.innerHTML = readyCard;
+};
+
+function createCountrieList(country) {
+    clearAll();
+    const readyList = country.map((c) => 
+        `<li class="country-list--item">
+            <img src="${c.flags.svg}" alt="Country flag" width="40", height="30">
+            <span class="country-list--name">${c.name.official}</span>
+        </li>`)
+        .join("");
+    list.insertAdjacentHTML('beforeend', readyList);
+};
+
+function clearAll() {
+  list.innerHTML = '';
+  div.innerHTML = '';
+};
